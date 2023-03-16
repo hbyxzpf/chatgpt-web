8b48b2404a3f4f796468d0e579bd6854ad5133b7
zhaopengfei <hbyx.zpf@gmail.com>
2023-03-16 15:42:29 +0800

    fix

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,12 +1,8 @@
 import type { APIRoute } from 'astro'
 import { generatePayload, parseOpenAIStream } from '@/utils/openAI'
 import { verifySignature } from '@/utils/auth'
-// #vercel-disable-blocks
-import { fetch, ProxyAgent } from 'undici'
-// #vercel-end
 
 const apiKey = import.meta.env.OPENAI_API_KEY
-const httpsProxy = import.meta.env.HTTPS_PROXY
 const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/,'')
 const sitePassword = import.meta.env.SITE_PASSWORD
 
@@ -23,13 +19,6 @@ export const post: APIRoute = async (context) => {
     return new Response('Invalid signature')
   }
   const initOptions = generatePayload(apiKey, messages)
-  // #vercel-disable-blocks
-  if (httpsProxy) {
-    initOptions['dispatcher'] = new ProxyAgent(httpsProxy)
-  }
-  // #vercel-end
-
-  // @ts-ignore
   const response = await fetch(`${baseUrl}/v1/chat/completions`, initOptions) as Response
 
   return new Response(parseOpenAIStream(response))
